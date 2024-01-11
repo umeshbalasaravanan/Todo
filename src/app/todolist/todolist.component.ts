8597fb99e789873312a2a1f250e0a2624ba6b573
umeshbalasaravanan <umesh.balasaravanan@gmail.com>
2024-01-11 19:25:03 +0530

    implemented refresh todos

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoDataService } from '../services/data/todo-data.service';
 import { ActivatedRoute } from '@angular/router';
-export class Todo{
+export class Todo {
   constructor(public id: number,
     public description: string,
     public done: boolean,
-    public targetDate: Date){
+    public targetDate: Date) {
   }
 }
 @Component({
@@ -14,7 +14,7 @@ export class Todo{
   styleUrls: ['./todolist.component.css']
 })
 export class TodolistComponent implements OnInit {
-  
+
   constructor(private todoDataService: TodoDataService) { }
 
   // todo = {
@@ -29,6 +29,9 @@ export class TodolistComponent implements OnInit {
   //   new Todo(3, "sleep well", true, new Date())
   // ]
   ngOnInit(): void {
+    this.refreshTodos();
+  }
+  refreshTodos() {
     let user = sessionStorage.getItem('authenticatedUser')
     console.log("user after entering into todos page: " + user)
     this.todoDataService.retrieveTodoData(user).subscribe(
@@ -43,7 +46,8 @@ export class TodolistComponent implements OnInit {
     this.todoDataService.deleteTodoData(user, id).subscribe(
       response => {
         this.message = `Deletion of Todo ${id} is successful`
+        this.refreshTodos()
       }
     )
-    }
+  }
 }
